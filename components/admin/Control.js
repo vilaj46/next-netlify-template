a6adbac1a14918ffd0984ab9b6b2447d6031052a
Julian <vilaj46@gmail.com>
2021-12-31 18:29:47 -0500

    testing

--- components/admin/Control.js
+++ components/admin/Control.js
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import React from "react";
 
+// https://www.netlifycms.org/docs/custom-widgets/#advanced-field-validation
+
 export default class Control extends React.Component {
   static propTypes = {
     onChange: PropTypes.func.isRequired,
@@ -10,19 +12,47 @@ export default class Control extends React.Component {
   };
 
   static defaultProps = {
-    value: "",
+    value: {
+      one: "",
+      two: "",
+    },
+  };
+
+  state = {
+    ...this.props.value,
+  };
+
+  inputChange = (e) => {
+    const newValue = e.target.value;
+    const { name } = e.target;
+
+    const { value, onChange } = this.props;
+
+    const temp = { ...value };
+    temp[name] = newValue;
+
+    this.setState({
+      ...this.state,
+      [name]: newValue,
+    });
+
+    onChange(JSON.stringify(temp));
   };
 
   render() {
     const { forID, value, onChange, classNameWrapper } = this.props;
-    console.log(value);
+
+    const { one } = this.state;
+
     return (
       <input
         type="text"
         id={forID}
         className={classNameWrapper}
-        value={value || ""}
-        onChange={(e) => onChange(e.target.value)}
+        value={one || ""}
+        name="one"
+        onChange={(e) => this.inputChange(e)}
+        // onChange={(e) => onChange(e.target.value)}
       />
     );
   }
